c31339d71d721a86eab9bf30d5fc601537c8a669
PRONATEC PRONATEC <pronatec@LABS.GARANHUNS.IFPE>
2018-08-31 14:49:30 -0300

    ai

---
+++ src/br/edu/ifpe/java8/ex1/MainEx1.java
@@ -0,0 +1,10 @@
+package br.edu.ifpe.java8.ex1;
+
+public class MainEx1 {
+
+	public static void main(String[] args) {
+		RelatorioAlunos1.gerar(TipoRelatorio1.TODOS);
+		
+		
+	}
+}
---
+++ src/br/edu/ifpe/java8/ex3/TipoRelatorio3.java
@@ -0,0 +1,7 @@
+package br.edu.ifpe.java8.ex3;
+
+public enum TipoRelatorio3 {
+	MEDIA_GERAL,
+	QTDE_ALUNOS_ACIMA_MEDIA,
+	QTDE_ALUNOS_ABAIXO_MEDIA;
+}
